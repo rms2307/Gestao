099ab88c2758ad271a8c8506362263a641bd054a
Robson de Moraes Silva <rms2307@outlook.com>
2024-11-07 09:10:34 -0300

    Implementa envio de e-mail para confirmação de cadastro

---
+++ Gestao/Gestao/Infra/Mail/EmailSender.cs
@@ -0,0 +1,47 @@
+﻿using Gestao.Data;
+using Microsoft.AspNetCore.Identity;
+using System.Net.Mail;
+
+namespace Gestao.Infra.Mail
+{
+    public class EmailSender(ILogger<EmailSender> logger, SmtpClient smtpClient, IConfiguration configuration) : IEmailSender<ApplicationUser>
+    {
+        private readonly ILogger logger = logger;
+        private readonly SmtpClient smtpClient = smtpClient;
+        private readonly IConfiguration configuration = configuration;
+
+        public Task SendConfirmationLinkAsync(ApplicationUser user, string email,
+            string confirmationLink) => SendEmailAsync(email, "Confirm your email",
+            "Please confirm your account by " +
+            $"<a href='{confirmationLink}'>clicking here</a>.");
+
+        public Task SendPasswordResetLinkAsync(ApplicationUser user, string email,
+            string resetLink) => SendEmailAsync(email, "Reset your password",
+            $"Please reset your password by <a href='{resetLink}'>clicking here</a>.");
+
+        public Task SendPasswordResetCodeAsync(ApplicationUser user, string email,
+            string resetCode) => SendEmailAsync(email, "Reset your password",
+            $"Please reset your password using the following code: {resetCode}");
+
+        public async Task SendEmailAsync(string toEmail, string subject, string message)
+        {
+            await Execute(subject, message, toEmail);
+        }
+
+        public async Task Execute(string subject, string message, string toEmail)
+        {
+            MailMessage mailMessage = new()
+            {
+                From = new MailAddress(configuration.GetValue<string>("EmailSender:User")!),
+                Subject = subject,
+                Body = message,
+                IsBodyHtml = true
+            };
+            mailMessage.To.Add(new MailAddress(toEmail));
+
+            await smtpClient.SendMailAsync(mailMessage);
+
+            logger.LogInformation("Email to {EmailAddress} sent!", toEmail);
+        }
+    }
+}

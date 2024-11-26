ba35ef7c5f60e744c68ce0010c6e89ff16ee2f37
Robson de Moraes Silva <rms2307@outlook.com>
2024-11-26 07:40:43 -0300

    Faz paginação de Company

---
+++ Gestao/Gestao.Client/Libraries/Utilities/PaginatedList.cs
@@ -0,0 +1,11 @@
+﻿namespace Gestao.Client.Libraries.Utilities
+{
+    public class PaginatedList<T>(List<T> items, int pageIndex, int totalPages)
+    {
+        public List<T> Items { get; } = items;
+        public int PageIndex { get; } = pageIndex;
+        public int TotalPages { get; } = totalPages;
+        public bool HasNextPage => PageIndex < TotalPages;
+        public bool HasPreviousPage => PageIndex > 1;
+    }
+}
--- Gestao/Gestao/Data/Repositories/CompanyRepository.cs
+++ Gestao/Gestao/Data/Repositories/CompanyRepository.cs
@@ -1,6 +1,50 @@
-﻿using Gestao.Domain;
+﻿using Gestao.Client.Libraries.Utilities;
+using Gestao.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace Gestao.Data.Repositories
 {
+    public class CompanyRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CompanyRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<PaginatedList<Company>> GetAllAsync(Guid applicationUserId, int pageIndex, int pageSize)
+        {
+            List<Company> items = await _db.Companies
+                .Where(a => a.UserId == applicationUserId)
+                .Skip((pageIndex - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            int count = await _db.Companies.Where(a => a.UserId == applicationUserId).CountAsync();
+            int totalPages = (int)Math.Ceiling((decimal)count / pageSize);
+
+            return new PaginatedList<Company>(items, pageIndex, totalPages);
+        }
+
+        public Company Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Company company)
+        {
+
+        }
+
+        public void Update(Company company)
+        {
+
+        }
+
+        public void Delete(int id)
+        {
+
+        }
     }
 }

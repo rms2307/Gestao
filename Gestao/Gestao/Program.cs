ab34e094d9ffe655352d85ba579df5869bc46354
Robson de Moraes Silva <rms2307@outlook.com>
2024-12-10 23:07:26 -0300

    Add endpoints

--- Gestao/Gestao/Program.cs
+++ Gestao/Gestao/Program.cs
@@ -1,12 +1,15 @@
 using Gestao.Components;
 using Gestao.Components.Account;
 using Gestao.Data;
+using Gestao.Data.Repositories.Interfaces;
+using Gestao.Data.Repositories;
 using Gestao.Infra.Mail;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System.Net;
 using System.Net.Mail;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,10 +24,10 @@ builder.Services.AddScoped<IdentityRedirectManager>();
 builder.Services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
 
 builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultScheme = IdentityConstants.ApplicationScheme;
-        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-    })
+{
+    options.DefaultScheme = IdentityConstants.ApplicationScheme;
+    options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+})
     .AddIdentityCookies();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
@@ -60,6 +63,8 @@ builder.Services.AddAuthentication()
 
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, EmailSender>();
 
+AddRepositories(builder.Services);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -88,4 +93,30 @@ app.MapRazorComponents<App>()
 // Add additional endpoints required by the Identity /Account Razor components.
 app.MapAdditionalIdentityEndpoints();
 
+MapEndpoints(builder, app);
+
 app.Run();
+
+static void AddRepositories(IServiceCollection services)
+{
+    services.AddScoped<IAccountRepository, AccountRepository>();
+    services.AddScoped<ICategoryRepository, CategoryRepository>();
+    services.AddScoped<ICompanyRepository, CompanyRepository>();
+    services.AddScoped<IDocumentRepository, DocumentRepository>();
+    services.AddScoped<IFinanacialTransactionRepository, FinanacialTransactionRepository>();
+}
+
+static void MapEndpoints(WebApplicationBuilder builder, WebApplication app)
+{
+    int pageSize = builder.Configuration.GetValue<int>("Pagination:PageSize");
+
+    app.MapGet("/api/categories", async (
+        ICategoryRepository repository,
+        [FromQuery] int companyId,
+        [FromQuery] int pageIndex) =>
+    {
+        var data = await repository.GetAllAsync(companyId, pageIndex, pageSize);
+
+        return Results.Ok(data);
+    });
+}
